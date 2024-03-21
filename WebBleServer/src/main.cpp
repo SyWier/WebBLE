745b6ccee6d418518b623d0893c09698a27fe8f2
SyWier <SyWier@gmail.com>
2024-03-22 00:18:35 +0100

    Tracking server files

---
+++ WebBleServer/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+//#include <EzBLE.h>
+#include <Web.h>
+
+WebServer webServer;
+
+const int buttonPin = 10;
+
+void setup() {
+    Serial.begin(115200);
+    // delay(10000);
+
+    // EzBLE EzBLE;
+    // EzBLE.Init();
+    // EzBLE.SetPacketValue("Hello BLE");
+    // EzBLE.Start();
+
+    webServer.Init();
+
+    pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+    int val = digitalRead(buttonPin);
+    String str = "Button state: ";
+    Serial.println(str + val);
+    webServer.Update();
+    delay(1000);
+}
